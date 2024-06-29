1eb21750b7a324dbc51b75c349908439033d3151
Masahiro Obuchi <o.masahiro0328@gmail.com>
2024-06-29 23:48:43 +0900

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-# Create symbolic link
+set -u
+
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+
+# Create symbolic link of dotfiles
+for f in .??*; do
+	[ "$f" = ".git" ] && continue
+	[ "$f" = ".gitconfig.local.template" ] && continue
+	[ "$f" = ".gitmodules" ] && continue
+	[ "$f" = ".config" ] && continue
+
+	ln -snfv ${PWD}/"$f" ~/
+done
